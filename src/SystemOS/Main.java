3393119ca70b92595e913effb4f1c8da2105e2ba
Isaque Ferrea <isaqueprf10@gmail.com>
2025-11-01 16:04:46 -0300

    Version 1.0.0

--- src/SystemOS/Main.java
+++ src/SystemOS/Main.java
@@ -2,7 +2,7 @@ package SystemOS;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste");
-        System.out.println("Teste2");
+        System.out.println("Versão 2");
+
     }
 }
